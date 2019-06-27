119f0cb2ea247d18bae0e12b6078e563c99258b9
Rachele Suy <rachelesuy@gmail.com>
2019-06-27 10:14:12 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -12,7 +12,7 @@ class Artist < ActiveRecord::Base
     self.songs.count
   end
 
-  def genre_countl
+  def genre_count
     #return the number of genres associated with the artist
     genres = Songs.collect do |song|
        song.genre
